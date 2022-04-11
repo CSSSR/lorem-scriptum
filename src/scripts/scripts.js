d20f818f3fb1e72bfbca732fa03e6c90a0b35ad4
d.batmanov <dmitriy.batmanov@csssr.com>
2022-04-11 16:36:38 +0300

    feat: LS-101: added integration with amo

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,94 @@
+const sendApplication = async ({ email, phone, message }) => {
+  await fetch('https://master.com.csssr.cloud/api/submit-form', {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      phone,
+      message,
+      name: 'Первичный запрос с Lorem Scriptum',
+      pageName: 'contactUs',
+      language: 'en',
+    }),
+  })
+}
+
+const formContainer = document.getElementById('formContainer')
+const applicationForm = document.getElementById('applicationForm')
+const submitButton = document.querySelector('#applicationForm .applicationForm__submitButton')
+const emailField = document.getElementById('applicationFormEmail')
+const messageField = document.getElementById('applicationFormMessage')
+
+const getErrorMessage = (field) => {
+  let message = null
+  const { valueMissing, typeMismatch } = field.validity
+
+  if (valueMissing) {
+    message = 'Fill out the field to send a message'
+  }
+
+  if (typeMismatch) {
+    message = 'Invalid e-mail address'
+  }
+
+  return message
+}
+
+const setErrorState = (field, message) => {
+  field.setCustomValidity(' ')
+
+  field.classList.add('input__type_invalid')
+  field.nextSibling.innerText = message
+}
+
+const resetErrorState = (field) => {
+  if (field.classList.contains('input__type_invalid')) {
+    field.setCustomValidity('')
+
+    field.classList.remove('input__type_invalid')
+    field.nextSibling.innerText = ''
+  }
+}
+
+const handleErrorState = (evt) => {
+  evt.preventDefault()
+
+  setErrorState(evt.target, getErrorMessage(evt.target))
+}
+
+emailField.addEventListener('input', ({ target }) => resetErrorState(target))
+messageField.addEventListener('input', ({ target }) => resetErrorState(target))
+
+emailField.addEventListener('invalid', handleErrorState)
+messageField.addEventListener('invalid', handleErrorState)
+
+applicationForm.addEventListener('submit', async (evt) => {
+  try {
+    evt.preventDefault()
+
+    const form = evt.target
+
+    const { email, phone, message } = form.elements
+
+    submitButton.setAttribute('disabled', 'disabled')
+
+    await sendApplication({
+      email: email.value,
+      phone: phone.value,
+      message: message.value,
+    })
+
+    formContainer.classList.add('applicationForm--state-success')
+  } catch (err) {
+    console.error(err)
+  } finally {
+    submitButton.removeAttribute('disabled')
+  }
+})
+
 const cookies = document.cookie.split(';').map((item) => item.trim())
 
 const COOKIE_ACCEPTED_KEY = 'COOKIE_ACCEPTED'
